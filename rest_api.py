93ab6535fc61e35dd44e6ab6765a6d0e5739e8b6
SHAIK MOHAMMED SIDDIQ <shaiksiddiq8004@gmail.com>
2026-05-09 14:25:43 +0530

    Add files via upload

---
+++ rest_api.py
@@ -0,0 +1,12 @@
+my_details = {
+    "name": "Shaik Siddiq",
+    "college": "Krishna Chaitanya Institute of Technology and Sciences",
+    "branch": "B.Tech CSE",
+    "cgpa": "6.93",
+    "city": "Hyderabad",
+    "skills": ["Python", "REST API", "GitHub"],
+    "mobile number":"9390598004",
+    "hobbies": "exploring new things",
+}
+
+print(my_details)
